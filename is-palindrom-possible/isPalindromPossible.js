28b3cfedd5d0ee33d0451523e60d79ec45d28197
Shreyansh Narendrabhai <shreyanshnarendrabhai@Shreyanshs-MacBook-Pro.local>
2020-03-14 14:00:37 -0400

    Update the algorithm

--- is-palindrom-possible/isPalindromPossible.js
+++ is-palindrom-possible/isPalindromPossible.js
@@ -1,15 +1,12 @@
-const isPalindromPossible = str => {
-  let count = {};
-  for (let s of str) {
-    if (count[s]) {
-      delete count[s];
-    } else {
-      count[s] = true;
-    }
-  }
-  let leng = Object.keys(count).length;
-  if (leng > 1) {
+const isPalindrom = str => {
+  if (!str) {
     return false;
+  }
+  let n = str.length;
+  for (let i = 0; i < n / 2; i++) {
+    if (str[i] !== str[n - i - 1]) {
+      return false;
+    }
   }
   return true;
 };
