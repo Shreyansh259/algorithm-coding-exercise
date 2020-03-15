c3af6eb06ee07a4c0a874dba5c8fd3cf2a24714a
Shreyansh Narendrabhai <shreyanshnarendrabhai@Shreyanshs-MacBook-Pro.local>
2020-03-14 20:29:27 -0400

    Initial commit

--- is-palindrom-possible/isPalindromPossible.js
+++ is-palindrom-possible/isPalindromPossible.js
@@ -1,15 +1,15 @@
 const isPalindromPossible = str => {
   let count = {};
-  for(let s of str){
-      if(count[s]){
-          delete count[s];
-      } else {
-          count[s] = true;
-      }
+  for (let s of str) {
+    if (count[s]) {
+      delete count[s];
+    } else {
+      count[s] = true;
+    }
   }
   let leng = Object.keys(count).length;
-  if(leng > 1){
-      return false;
+  if (leng > 1) {
+    return false;
   }
   return true;
-}
+};
---
+++ is-palindrom/README.md
@@ -0,0 +1,16 @@
+
+# Check if given string is palindrom
+
+You have given a string to check if it is palindrom or not.
+
+e.g. 
+```
+Input:'abba'
+
+Output: true
+```
+```
+Input: 'abbc'
+
+Output: false
+```
